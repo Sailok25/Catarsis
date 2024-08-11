9826a067d7e519f0e5fe815d0f68433f558e7255
Sailok <127998789+Sailok25@users.noreply.github.com>
2024-08-11 13:09:13 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const conjuntoFotos = document.getElementById('conjunto-fotos');
-    const rutaCarpeta = 'img/sections/galeria/';
-    const numImagenes = 10;
+    const rutaCarpeta = 'img/sections/galeria/random/';
+    const numImagenes = 4;
 
     for (let i = 1; i <= numImagenes; i++) {
         const img = document.createElement('img');
-        img.src = `${rutaCarpeta}f${i}.jpeg`;
+        img.src = `${rutaCarpeta}${i}.jpg`;
         img.alt = `Imagen ${i} tomada por el grupo Catarsis`;
         img.classList.add('caja-foto');
         conjuntoFotos.appendChild(img);
